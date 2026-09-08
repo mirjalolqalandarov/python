6890a8baa4a72985336b71d54f7f3fdfbe3815ea
MIRJALOL <a0000@0000ui-MacBookAir.local>
2026-09-08 08:56:52 -0700

    1-lesson yangilandi

--- 1-lesson.py
+++ 1-lesson.py
@@ -8,3 +8,8 @@ narx = 25000
 soni = int (input("nechta non ?"))
 natija = soni* narx
 print("umumiy narx:" , natija)
+
+narx = 350000
+soni = int (input("nechta kitob olishingiz kerak"?))
+natija = soni*narx
+print("umumiy narx:" , natija)
