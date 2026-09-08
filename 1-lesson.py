343651f9a55c098cecf32c65409231b04d048a4d
MIRJALOL <a0000@0000ui-MacBookAir.local>
2026-09-08 08:22:43 -0700

    1-lesson yangilandi

--- 1-lesson.py
+++ 1-lesson.py
@@ -1,1 +1,10 @@
 
+narx = 120000
+soni = int (input("Nechta to'p?"))
+natija = soni* narx
+print("Umimiy narx:" , natija)
+
+narx = 25000
+soni = int (input("nechta non ?"))
+natija = soni* narx
+print("umumiy narx:" , natija)
