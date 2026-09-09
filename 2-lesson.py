050dd9cf35599ed3fe7e4b9e92fe576f760e3540
MIRJALOL <a0000@0000ui-MacBookAir.local>
2026-09-09 08:56:03 -0700

    2-lesson yangilandi

---
+++ 2-lesson.py
@@ -0,0 +1,67 @@
+# 1 chi misol
+non = 4000
+sut = 3000
+jami = non+sut
+print(jami)
+# 2 chi misol
+qalam = 2000
+soni = 5
+jami = qalam*soni
+print(jami)
+# 3 chi misol
+pul = 50000
+kitob = 17000
+qoldiq = pul-kitob
+print(qoldiq)
+# 4 chi misol
+olma = 12000
+banan = 15000
+jami = olma*4+banan*1
+print(jami)
+# 5 chi misol
+har_kuni = 10000 
+ jami_kunlar_soni = 7
+ sarflagan_puli = Har_kuni*jami kunlar_soni
+ print(sarflagan_puli)
+ # 6 chi misol
+ jami_konfet = 48
+ bolalar_soni=6
+ taqsimlash = jami_konfet/bolalar_soni
+ print(taqsimlash)
+# 7 ci misol
+daqiqa=125
+soat = 125 // 60
+daqiqa = 125 % 60
+print("soat",soat)
+print("daqiqa",daqiqa)
+# 8 ci misol
+ppul = 25675
+som = (25675 // 1000) * 1000
+#shu hatorda 25 ni izina 1000 qoshishni ozim oylap tapdim 😂
+tiyn = 25675 % 100
+print("som" , som)
+print("tiyn" , tiyn)
+# 9 ci misol
+limon = 18050
+necci_kg = 6
+jami = limon * necci_kg
+print(jami)
+som = (18050 % 1000) * 1000
+tiyn = 18050 // 1000
+print("som", som)
+print("tiyn" , tiyn)
+#shu misolni ozim oylap tapdim polniy tushinib 1 cida hatosiz ciqardim😎
+# 10 ci misol
+pul = 100000
+daftar = 7000
+ruchka = 3000
+kitob  = 25500
+soni_d = 3
+soni_r = 2
+soni_k = 1
+jami = daftar * soni_d + ruchka * soni_r + kitob * soni_k
+print(jami)
+qoldiq = pul - jami
+print(qoldiq)
+har_biriga = qoldiq // 4
+print(har_biriga)
