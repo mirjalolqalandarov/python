381d989b0a46d1dd1239801f661975a32ca70ba4
MIRJALOL <a0000@0000ui-MacBookAir.local>
2026-09-09 04:10:20 -0700

    1-lesson yangilandi

--- 1-lesson.py
+++ 1-lesson.py
@@ -1,29 +1,29 @@
-
+#1-chi misol
 narx = 120000
 soni = int (input("Nechta to'p?"))
 natija = soni* narx
 print("Umimiy narx:" , natija)
-
+#2-chi misol
 narx = 25000
 soni = int (input("nechta non ?"))
 natija = soni* narx
 print("umumiy narx:" , natija)
-
+# 3 chi misol
 narx = 350000
-soni = int (input("nechta kitob olishingiz kerak"?))
+soni = int (input("nechta kitob olishingiz kerak?"))
 natija = soni*narx
 print("umumiy narx:" , natija)
-
+#4 chi misol
 narx = 20000
 soni = int (input("Nechta daftar"))
 natija = soni*narx
 print("umumiy narx:" ,natija)
-
+# 5 ci misol
 narx = 120000
 soni = int(input("nechta salfetka?"))
 natija=soni*narx
 print("umumiy narx:" , natija)
-
+# 6 chi misol
 narx = 150000
 soni = int(input("Necha kun ishladingiz? "))
 bonus = int(input("Bonus qancha? "))
@@ -32,7 +32,7 @@ jami = daromad + bonus
 print("Ish haqi:", daromad)
 print("Bonus:", bonus)
 print("Umumiy daromad:", jami)
-
+# 7 chi misol
 non =  4000
 sut =  1200
 tuxum = 1500
@@ -42,8 +42,27 @@ tuxum_soni = int(input("nechta tuxum olasiz/"))
 non_narxi = non_soni*non
 sut_narxi = sut_soni*sut
 tuxum_narxi = tuxum_soni*tuxum
-jami = non_narxi+sut_narxi+tuxum narxi
+jami = non_narxi+sut_narxi+tuxum_narxi
 print("non narxi:" , non_narxi)
 print("sut narxi:"  ,sut_narxi)
 print("tuxum narxi:" , tuxum_narxi)
 print("Jami narx ;" , jami)
+# 8 chi misol
+a = 10
+b = 20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+#9 chi misol
+talaba = {
+    "ism": "Mirjalol",
+    "yosh" :19,
+    "fan": "Python",
+    "yashash joyi":"yunusobod",
+    }
+    print(talaba["Mirjalol"])
+    print(talaba["fan"])
+    print(talaba["yashash joyi"])
+    print(talaba["yosh"])
+    print(talaba)
