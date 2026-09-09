f3767b4bf4001070c7de73062657efa4789a5c05
MIRJALOL <a0000@0000ui-MacBookAir.local>
2026-09-08 22:27:18 -0700

    1 va 2 lesson yangilandi

--- 1-lesson.py
+++ 1-lesson.py
@@ -13,3 +13,37 @@ narx = 350000
 soni = int (input("nechta kitob olishingiz kerak"?))
 natija = soni*narx
 print("umumiy narx:" , natija)
+
+narx = 20000
+soni = int (input("Nechta daftar"))
+natija = soni*narx
+print("umumiy narx:" ,natija)
+
+narx = 120000
+soni = int(input("nechta salfetka?"))
+natija=soni*narx
+print("umumiy narx:" , natija)
+
+narx = 150000
+soni = int(input("Necha kun ishladingiz? "))
+bonus = int(input("Bonus qancha? "))
+daromad = soni * narx
+jami = daromad + bonus
+print("Ish haqi:", daromad)
+print("Bonus:", bonus)
+print("Umumiy daromad:", jami)
+
+non =  4000
+sut =  1200
+tuxum = 1500
+non_soni = int(input("nechta non olasiz?"))
+sut_soni = int(input("necta sut olasiz"))
+tuxum_soni = int(input("nechta tuxum olasiz/"))
+non_narxi = non_soni*non
+sut_narxi = sut_soni*sut
+tuxum_narxi = tuxum_soni*tuxum
+jami = non_narxi+sut_narxi+tuxum narxi
+print("non narxi:" , non_narxi)
+print("sut narxi:"  ,sut_narxi)
+print("tuxum narxi:" , tuxum_narxi)
+print("Jami narx ;" , jami)
