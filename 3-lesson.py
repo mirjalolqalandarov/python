02a20eda34bab4430d3b2a5b3bbb436277a85f29
mirjalolqalandarov <mirjalolqalandarov539@gmail.com>
2026-09-09 23:46:30 -0700

    3-lesson yangilandi

--- 3-lesson.py
+++ 3-lesson.py
@@ -1,1 +1,36 @@
-mirjalol
+# 1 ci misol
+s = input("ismingiz nima?")
+print(s)
+# 2 ci misol
+s = input("Kiriting:")
+print('salom {}'.format(s))
+# 3 ci misol
+ism = input("Ism: ")
+familiya = input("Familiya: ")
+print("{} {}".format(ism, familiya))
+# 4 ci misol
+s = input("soz kiriting:")
+print(len(s))#format kerak emas len()ozi yetarli boladi
+# 5 ci misol
+s = input("soz kiriting:")
+print(s.upper())#bu yerda toliq katta harifga otqazadigan funkisya s.upper()
+# 6 ci misol
+s = input("sozni kiritsangiz boladi:")
+print(s.lower())#Bu yerda ㅏㅑ kicik harif qiladigan S.LOWER()
+# 7 ci misol
+s = input("Ismingizni kiriting:")
+print(s[0].format())#bunda nimadan foydalandim string tioida har bir belgi harf alohida yacheykada boladi men birinci harfimni ciqarishim ucin 0 ci indeksga murojat qilishim kerak
+# 8 ci misol
+s = input("sozni kiriting:")
+print(s[-1].format())#bunda esa oxirgi harfini ciqarish
+# 9 ci misol
+s = input("sozni kirting:")
+print(s.replace("salom","assalomu eleykum"))#bunda sozni orniga boshqa sozga almashtirish boldi
+# 10 ci misol
+ism = input("ism:")
+Familya = input("Familya:")
+print("{} {}". format(ism,Familya))
+print(ism.upper())
+print(len(ism))
+print(ism[0])# Bu 10 ci misolda ishlagan misollarimni deyarli hamasindan foydalangan
+
