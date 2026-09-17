be93aa1b8ac1e4d9cf9da7594e92fce77fe53b40
mirjalolqalandarov <mirjalolqalandarov539@gmail.com>
2026-09-17 10:21:05 -0700

    Yangilandi

--- 4-lesson.py
+++ 4-lesson.py
@@ -1,5 +1,57 @@
-narx = 12000
-soni = 3
-jami = narx * soni
-print(jami)
+#1 ci misol 
+fam = input('familyangizni kiriting')
+ism = input('ismingizni kiriting')
+if ism or fam:
+    print('tashakkur') 
+else:
+    print('xato yozding')
+#2 ci misol
+a = 12
+b = 10
+s = a * b
+print("yuzasi",s)
+# 3 ci misol
+a = 17
+S = a * a
+print("Yuzasi",s)
+#4 ci misol 
+a = 12
+b = 10
+c = 14
+Ortaarfimetik = (a + b +c) / 3
+print("Orta arfimetik",Orta_arfimetik)
+#5 ci misol
+km = 10
+metr = km * 1000
+print("metr", metr)
+# 6 ci misol
+a = 4
+kvadrati = a ** 2
+kubi = a ** 3
+print("kvadrati",kvadrati)
+print("kubi",kubi)
+#7 ci misol
+a = 13
+h = 2
+S = a * h / 2
+print("Togri tortburchak yuzi", S)
+# 8 ci misol
+R = 4 
+Doimiy = 3.14
+S = 2 * Doimiy * R
+print("Doira yuzi", S)
+# 9 ci misol ikki sonni onni olmoshadi
+a = 15
+b = 20
+a, b = b, a 
+print("a =",a)
+print("b =",b)
+# 10 ci misol
+
+
+
+
+
+ 
+    
 
--- 5-lesson.py
+++ 5-lesson.py
@@ -1,4 +1,33 @@
-narx = 12000
-soni = 3
-jami = narx * soni
-print(jami)
+# 1 ci misol
+son = int(input("sonni kiriting?"))
+if son > 0:
+    print("musbat")
+else:
+    print('manfiy')
+# 2 ci misol
+son = int(input("sonni kiriting?"))
+if son / 2 ==0 :
+    print("juft son")
+else:
+    print("toq son")
+# 3 ci misol
+yoshi = int(input("Yoshingiz neccida?"))
+if yoshi > 18:
+    print("voyaga yetgan😎")
+else:
+    print("voyaga yetmagan detiski😭")
+# 3 ci misolni hazatib ishladimey shora galib polniy tushindfim
+# 4 ci misol
+ball = int(input("balingizni kiriting😊"))
+if ball > 60 :
+    print("siz imtihondan yaxshi otdingiz👌")
+else :
+    print("afsuski keyingi yilga qoldingiz sibalsiya🫡")
+# 5 ci misol
+a = int(input("Birinchi sonni kiriting?"))
+b = int(input("Ikkinchi sonni kiriting?"))
+if a > b :
+    print("birinchi son katta")
+else :
+    print("ikkinchi son katta")
+    
