f058fef3ab19a9688cac62c8182894f8f2c8d37f
mirjalolqalandarov <mirjalolqalandarov539@gmail.com>
2026-09-22 23:19:20 -0700

    yangilandi

---
+++ 7-lesson.py
@@ -0,0 +1,46 @@
+# 1 ci misol
+son = 1
+while son <= 5 :
+    print(son)
+    son += 1
+#2 ci misol
+son = 1
+while son <= 10 :
+    print(son)
+    son += 1
+# 3 ci misol
+son = 2
+while son <=10 :
+    print(son)
+    son+=2
+# 4 ci misol
+son = 10 
+while son >= 1:
+    print(son)
+    son -=1
+# 5 ci misol
+son = 1
+yigindi = 0
+while son <=10:
+    yigindi +=son
+    son +=1
+print(yigindi)
+#6 ci misol
+son = 1
+while son <=20:
+    print(son)
+    son += 2
+# 7 ci misol
+son =int (input("sonni kiriting"))
+Son = 1
+while Son <= son:
+    print(Son)
+    Son += 1#shu misolni ozim oylab tapdim
+# 8 ci misol
+son = 1
+while son <=50 :
+      if son % 3 == 0:
+          print(son)
+      son += 1
+# 9 ci misol
+
